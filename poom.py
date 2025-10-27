023d7f441ca35538e4260baf2fe5bb15b0a70ead
Dongwook Kim <dok9612@gmail.com>
2025-10-27 15:55:52 -0400

    'updates'

--- poom.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd  # We'll install this soon
-
-print("Hello, World!")
-
-# Create a simple DataFrame with pandas
-data = {"Name": ["Alice", "Bob"], "Age": [25, 30]}
-df = pd.DataFrame(data)
-print(df)
